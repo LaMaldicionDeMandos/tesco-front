72744a09322f1d664c4f886fef4a856359f28fbb
Marcelo <marcelo.pasut@etermax.com>
2019-07-29 14:06:29 -0300

    trabajo en el popup de nueva cuenta

--- tesco-front-app/src/app/accounts/accounts.component.ts
+++ tesco-front-app/src/app/accounts/accounts.component.ts
@@ -1,4 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-account-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Nueva Cuenta</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <input id="accountNumber" class="form-control" placeholder="Número de cuenta">
+      <div class="row">
+        <div ngbDropdown class="d-inline-block">
+          <button class="btn" id="currency" ngbDropdownToggle>Moneda</button>
+          <div ngbDropdownMenu aria-labelledby="currency">
+            <button ngbDropdownItem>PESO</button>
+            <button ngbDropdownItem>DOLLAR</button>
+            <button ngbDropdownItem>EURO</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+export class AccountModalContentComponent {
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
 
 @Component({
   selector: 'app-accounts',
@@ -7,11 +41,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsComponent implements OnInit {
   private accounts: any[];
-  constructor() {
-    this.accounts = [{ id: 1, accountNumber: 1, currency: 'PESO', balance: 1000.4567}, { id: 2, accountNumber: 2, currency: 'EURO', balance: 100.633}]
+  constructor(private modalService: NgbModal) {
+    this.accounts = [
+      { id: 1, accountNumber: 1, currency: 'PESO', balance: 1000.4567},
+      { id: 2, accountNumber: 2, currency: 'EURO', balance: 100.633}];
   }
 
   ngOnInit() {
+  }
+
+  newAccount() {
+    const modalRef = this.modalService.open(AccountModalContentComponent);
+    modalRef.componentInstance.name = 'World';
   }
 
 }
--- tesco-front-app/src/app/app.module.ts
+++ tesco-front-app/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountsComponent } from './accounts/accounts.component';
+import { AccountsComponent, AccountModalContentComponent } from './accounts/accounts.component';
 
 import { registerLocaleData } from '@angular/common';
 import localeAr from '@angular/common/locales/es-AR';
@@ -13,14 +14,17 @@ registerLocaleData(localeAr, 'es-AR');
 @NgModule({
   declarations: [
     AppComponent,
+    AccountModalContentComponent,
     AccountsComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
+  exports: [AccountModalContentComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AccountModalContentComponent]
 })
-
 export class AppModule { }
